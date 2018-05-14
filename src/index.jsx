ebc8b13ba99b93e7cbb3858d3d2346fa78df6ab2
Thomas Siller <siller.thomas@gmx.net>
2018-05-14 14:14:04 +0200

    fix linting

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-/*import { Provider } from 'react-redux'
+/* import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import reducer from 'services/reducers'*/
+import reducer from 'services/reducers' */
 import App from "./containers/App/index.jsx";
 
-/*const store = createStore(
+/* const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && // eslint-disable-line no-underscore-dangle
   window.__REDUX_DEVTOOLS_EXTENSION__() // eslint-disable-line no-underscore-dangle
-)*/
+) */
 
 document.addEventListener("DOMContentLoaded", () => {
   ReactDOM.render(
