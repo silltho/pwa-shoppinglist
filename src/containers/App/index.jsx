539298a3959fe88c63caee1666daf0fe52d902c4
Thomas Siller <siller.thomas@gmx.net>
2018-05-14 15:08:27 +0200

    add test shoppingItems

--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import ShoppingList from 'components/ShoppingList'
+
+const defaultProps = {
+  shoppingItems: [
+    {
+      desription: 'article1',
+      done: false
+    },
+    {
+      desription: 'article2',
+      done: false
+    },
+    {
+      desription: 'article3',
+      done: false
+    },
+    {
+      desription: 'article4',
+      done: false
+    }
+  ]
+}
 
 class App extends React.Component {
   render() {
-    console.log('ggggg')
-    return <div>container</div>
+    return <ShoppingList {...defaultProps} />
   }
 }
 
