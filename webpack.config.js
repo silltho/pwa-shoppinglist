2d9f7504fede68e4295421b98180b929831c8246
Patrick Obermueller <pobermueller.mmt-b2015@fh-salzburg.ac.at>
2018-05-14 14:30:56 +0200

    Add Manifest file

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+
 module.exports = {
   entry: "./src/index.jsx",
   output: {
@@ -32,6 +34,22 @@ module.exports = {
     new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
+    }),
+    new WebpackPwaManifest({
+      name: "PWA-ShoopingList",
+      short_name: "ShoppingList",
+      theme_color: "#0fac78",
+      background_color: "#0fac78",
+      display: "standalone",
+      orientation: "portrait",
+      scope: "/",
+      start_url: "/",
+      icons: [
+        {
+          src: path.resolve("src/assets/icon.png"),
+          sizes: [72, 96, 128, 192, 256, 384, 512] // multiple sizes
+        }
+      ]
     })
   ]
 };
