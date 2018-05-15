f6f6547bb658c3f80bc12c67fbe123c1ae0bb3d7
Thomas Siller <siller.thomas@gmx.net>
2018-05-15 11:42:51 +0200

    implement Header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-import PropTypes from 'prop-types'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 
 class Header extends React.Component {
   render() {
-    return <div>container</div>
+    return (
+      <AppBar position="static" color="default">
+        <Toolbar>
+          <Typography variant="title" color="inherit">
+            ShoppingList
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    )
   }
 }
 
--- src/containers/App/index.jsx
+++ src/containers/App/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import Header from 'components/Header'
 import ShoppingList from 'components/ShoppingList'
 
 class App extends React.Component {
   render() {
-    return <ShoppingList />
+    return (
+      <React.Fragment>
+        <Header />
+        <ShoppingList />
+      </React.Fragment>
+    )
   }
 }
 
