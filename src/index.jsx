dfc713620fb9294666a6d2af13ed978e054355a0
Thomas Siller <siller.thomas@gmx.net>
2018-05-15 15:14:19 +0200

    implement material-ui

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-/* import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import reducer from 'config/reducers' */
+import runtime from 'serviceworker-webpack-plugin/lib/runtime'
 import App from './containers/App/index.jsx'
 
-/* const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && // eslint-disable-line no-underscore-dangle
-  window.__REDUX_DEVTOOLS_EXTENSION__() // eslint-disable-line no-underscore-dangle
-) */
+if ('serviceWorker' in navigator) {
+  const registration = runtime.register()
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,36 @@
 const path = require('path')
 const HtmlWebPackPlugin = require('html-webpack-plugin')
 const WebpackPwaManifest = require('webpack-pwa-manifest')
+const ServiceWorkerWebpackPlugin = require('serviceworker-webpack-plugin')
 
 const srcPath = path.resolve(__dirname, 'src')
 const distPath = path.resolve(__dirname, 'dist')
 const iconPath = path.resolve('src/assets/icon.png')
+const swPath = path.resolve(__dirname, 'src/sw.js')
+
+const htmlWebpackPluginImpl = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html'
+})
+const webpackPwaManifestImpl = new WebpackPwaManifest({
+  name: 'PWA-ShoppingList',
+  short_name: 'ShoppingList',
+  theme_color: '#0fac78',
+  background_color: '#0fac78',
+  display: 'standalone',
+  orientation: 'portrait',
+  scope: '/',
+  start_url: '/',
+  icons: [
+    {
+      src: iconPath,
+      sizes: [72, 96, 128, 192, 256, 384, 512] // multiple sizes
+    }
+  ]
+})
+const serviceWorkerWebpackPluginImpl = new ServiceWorkerWebpackPlugin({
+  entry: swPath
+})
 
 module.exports = {
   entry: './src/index.jsx',
@@ -40,25 +66,8 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html'
-    }),
-    new WebpackPwaManifest({
-      name: 'PWA-ShoppingList',
-      short_name: 'ShoppingList',
-      theme_color: '#0fac78',
-      background_color: '#0fac78',
-      display: 'standalone',
-      orientation: 'portrait',
-      scope: '/',
-      start_url: '/',
-      icons: [
-        {
-          src: iconPath,
-          sizes: [72, 96, 128, 192, 256, 384, 512] // multiple sizes
-        }
-      ]
-    })
+    htmlWebpackPluginImpl,
+    webpackPwaManifestImpl,
+    serviceWorkerWebpackPluginImpl
   ]
 }
