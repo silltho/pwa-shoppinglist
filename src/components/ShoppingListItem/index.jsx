d5ee6b4a417d4ad26d742905f5017e01b20699c4
Thomas Siller <siller.thomas@gmx.net>
2018-05-27 18:34:49 +0200

    Merge branch 'master' of github.com:silltho/pwa-shoppinglist
    # Conflicts:
    #	src/index.jsx

--- src/components/ShoppingListItem/index.jsx
+++ src/components/ShoppingListItem/index.jsx
@@ -7,12 +7,68 @@ import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction'
 import ListItemText from '@material-ui/core/ListItemText'
 import Checkbox from '@material-ui/core/Checkbox'
 import IconButton from '@material-ui/core/IconButton'
+import Icon from 'assets/icon.png'
 
 class ShoppingListItem extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      item: {}
+    }
+  }
+
+  componentDidMount() {
+    this.firebaseCallback = firebase
+      .child(this.props.itemKey)
+      .on('value', (snap) => {
+        this.setState({ item: snap.val() })
+        if (snap.val() === null) {
+          return this.displayNotification(
+            'Item removed',
+            this.state.item.description
+          )
+        }
+        if (snap.val().done && !this.state.item.done) {
+          return this.displayNotification(
+            'Item checked',
+            this.state.item.description
+          )
+        }
+        if (!snap.val().done && this.state.item.done) {
+          return this.displayNotification(
+            'Item unchecked',
+            this.state.item.description
+          )
+        }
+        return true
+      })
+  }
+
+  componentWillUnmount() {
+    firebase.off('value', this.firebaseCallback)
+  }
+
+  displayNotification = (title, body) => {
+    if (Notification.permission === 'granted') {
+      navigator.serviceWorker.getRegistration().then((reg) => {
+        const options = {
+          body,
+          icon: Icon,
+          vibrate: [100, 50, 100],
+          data: {
+            dateOfArrival: Date.now(),
+            primaryKey: 1
+          }
+        }
+        reg.showNotification(title, options)
+      })
+    }
+  }
+
   toggleDone = () => {
     firebase.child(this.props.itemKey).update({
-      description: this.props.item.description,
-      done: !this.props.item.done
+      description: this.state.item.description,
+      done: !this.state.item.done
     })
   }
 
@@ -21,7 +77,7 @@ class ShoppingListItem extends React.Component {
   }
 
   render() {
-    const { item } = this.props
+    const { item } = this.state
 
     return (
       <ListItem role={undefined} dense button onClick={this.toggleDone}>
@@ -38,8 +94,7 @@ class ShoppingListItem extends React.Component {
 }
 
 ShoppingListItem.propTypes = {
-  itemKey: PropTypes.string.isRequired,
-  item: PropTypes.object.isRequired
+  itemKey: PropTypes.string.isRequired
 }
 
 export default ShoppingListItem
